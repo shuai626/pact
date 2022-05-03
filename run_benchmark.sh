ff682a7513d43ede273fa3cb5f6deaa691d5275a
Shu-Huai Sean Lin <slin185@terpmail.umd.edu>
2022-05-03 13:11:43 -0400

    updated script

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -4,3 +4,7 @@ bash benchmark.sh &> $OUT
 
 # Print out build and query times for each program file in benchmark.sh
 grep "real" $OUT | awk -F 'm' '{print $2}' | awk -F 's' '{print $1}'
+
+# Print out compile code size (in bytes) for each program file in benchmark.sh
+# Can specify file size of .run executable or .s assembly script
+grep "Compiled code size" $OUT | awk -F ': ' '{print $2}' | awk -F ' ' '{print $1}'
